ec17dc5593afbf690d124b2885896ecd3d68cf85
micahjnr <89548053+micahjnr@users.noreply.github.com>
2026-07-19 13:10:59 +0100

    Implement UpdateManager in MainActivity
    Added UpdateManager to handle app updates on creation and cleanup on destroy.

--- android/app/src/main/java/app/netlify/vii_mbuni/twa/MainActivity.java
+++ android/app/src/main/java/app/netlify/vii_mbuni/twa/MainActivity.java
@@ -1,5 +1,25 @@
 package app.netlify.vii_mbuni.twa;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private UpdateManager updateManager;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        updateManager = new UpdateManager(this);
+        updateManager.checkForUpdate();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (updateManager != null) {
+            updateManager.cleanup();
+        }
+        super.onDestroy();
+    }
+}
